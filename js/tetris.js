7ce8a1abfe89757a9265650286118c4dbca834ce
Bruno A. Rolim <brolim@gmail.com>
2020-07-15 11:59:51 -0300

    lines being drawn on JS side
    - more reliable css for the board and block

--- js/tetris.js
+++ js/tetris.js
@@ -1,11 +1,27 @@
 var tetris = {
   board: $('.board'),
-  draw_board: function () {
+
+  draw_empty_board: function () {
+    var board_html = '';
+    board_html += '<div class="row" data-row="0"></div>';
+    board_html += '<div class="row" data-row="1"></div>';
+    board_html += '<div class="row" data-row="2"></div>';
+    board_html += '<div class="row" data-row="3"></div>';
+    board_html += '<div class="row" data-row="4"></div>';
+    board_html += '<div class="row" data-row="5"></div>';
+    board_html += '<div class="row" data-row="6"></div>';
+    board_html += '<div class="row" data-row="7"></div>';
+    board_html;
+    tetris.board.append(board_html)
+  },
+
+  draw_blocks: function () {
     tetris
       .board
-      .find('.row[data-row=0]')
-      .append('<div class="block">block</div>')
+      .find('.row[data-row=1]')
+      .append('<div class="block"></div>')
   }
 }
 
-tetris.draw_board()
+tetris.draw_empty_board()
+tetris.draw_blocks()
