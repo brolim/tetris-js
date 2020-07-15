c3bd3970bb50e17a94d41cea088e55febd7f393b
Bruno A. Rolim <brolim@gmail.com>
2020-07-15 12:31:06 -0300

    refactorings on board object to have its own size

--- js/tetris.js
+++ js/tetris.js
@@ -1,27 +1,27 @@
 var tetris = {
-  board: $('.board'),
+  board: {
+    el: $('.board'),
+    rows: 8,
+    cols: 1
+  },
+
   blocks: [
     [0, 0]
   ],
 
   draw_empty_board: function () {
     var board_html = '';
-    board_html += '<div class="row" data-row="0"></div>';
-    board_html += '<div class="row" data-row="1"></div>';
-    board_html += '<div class="row" data-row="2"></div>';
-    board_html += '<div class="row" data-row="3"></div>';
-    board_html += '<div class="row" data-row="4"></div>';
-    board_html += '<div class="row" data-row="5"></div>';
-    board_html += '<div class="row" data-row="6"></div>';
-    board_html += '<div class="row" data-row="7"></div>';
-    board_html;
-    tetris.board.html(board_html)
+    for (var i = 0; i < tetris.board.rows; i++) {
+      board_html += '<div class="row" data-row="' + i + '"></div>';
+    }
+    tetris.board.el.html(board_html)
   },
 
   draw_blocks: function () {
     tetris.blocks.forEach(function(block) {
       tetris
         .board
+        .el
         .find('.row[data-row=' + block[1] + ']')
         .append('<div class="block"></div>')
     })
@@ -43,7 +43,9 @@ var tetris = {
 
   update_time: function() {
     tetris.blocks.forEach(function(block) {
-      block[1] += 1
+      if (block[1] < tetris.board.rows - 1) {
+        block[1] += 1
+      }
     })
   }
 }
